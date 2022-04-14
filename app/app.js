3cc3a7573fd24d62dad056c30a3344619c6e681f
RamosFe <fedearamos97@gmail.com>
2022-04-14 15:25:38 -0300

    Added support for tests using Jest and Supertest.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 
-const PORT = 3000;
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Initial Setup');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+module.exports = app;
