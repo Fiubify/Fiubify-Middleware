b561995a2a8252134f152ea24708d4d354af89a8
RamosFe <fedearamos97@gmail.com>
2022-04-24 13:21:14 -0300

    Added proxy for the User microservice.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const proxy = require('express-http-proxy');
+const servicesConfig = require('./config/servicesConfig');
 
 const app = express();
+
+app.use('/auth', proxy(servicesConfig.auth));
+app.use('/user', proxy(servicesConfig.users));
 
 app.get('/', (req, res) => {
   res.send('Initial Setup');
---
+++ app/config/servicesConfig.js
@@ -0,0 +1,7 @@
+//TODO use db or ENV variables
+const servicesConfig = {
+  users: 'https://fiubify-users-staging.herokuapp.com/user',
+  auth: 'https://fiubify-users-staging.herokuapp.com/auth',
+};
+
+module.exports = servicesConfig;
