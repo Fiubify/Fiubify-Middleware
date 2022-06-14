31486f16a1c08419283cac4189f288a9ad94566c
RamosFe <fedearamos97@gmail.com>
2022-06-13 23:16:42 -0300

    Added metrics to middleware.

--- app/config/servicesConfig.js
+++ app/config/servicesConfig.js
@@ -3,6 +3,7 @@ const servicesConfig = {
   users: "https://fiubify-users-staging.herokuapp.com/",
   auth: "https://fiubify-users-staging.herokuapp.com/",
   contents: "https://fiubify-streamable-staging.herokuapp.com/",
+  metrics: "https://fiubify-metrics-staging.herokuapp.com/",
 };
 
 module.exports = servicesConfig;
